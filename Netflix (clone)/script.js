f6fa1a756d198c2eb61b2cc069c3cf786eccd25e
d68741546-maker <d68741546@gmail.com>
2026-06-10 14:07:47 -0400

    Add files via upload

---
+++ Netflix (clone)/script.js
@@ -0,0 +1,22 @@
+// --- EFECTO NAV-BAR ---
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('negro');
+    } else {
+        navbar.classList.remove('negro');
+    }
+});
+
+// --- FUNCIONALIDAD DE BOTONES ---
+const botonReproducir = document.querySelector('.btn-banner.play');
+const botonInfo = document.querySelector('.btn-banner.info');
+
+botonReproducir.addEventListener('click', () => {
+    alert('🎬 ¡Iniciando reproducción de Stranger Things...! (Lógica conectada con éxito)');
+});
+
+botonInfo.addEventListener('click', () => {
+    alert('ℹ️ Stranger Things: Creada por los hermanos Duffer. Temporadas disponibles: 4. Género: Ciencia ficción / Drama.');
+});
